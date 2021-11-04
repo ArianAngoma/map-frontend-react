9c3b1b548b0863c04c450d4ce3c1928bbe67efe8
unknown <arian.angoma@tecsup.edu.pe>
2021-11-03 23:33:14 -0500

    Obtener las coordenadas del mapa

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -3,17 +3,20 @@ import mapboxgl from 'mapbox-gl'
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX;
 
+/* Punto inicial del mapa */
 const startPoint = {
-    lng: 5,
-    lat: 34,
-    zoom: 2
+    lng: -77.0279,
+    lat: -12.0676,
+    zoom: 14
 }
 
 export const MapPage = () => {
-
     const mapDiv = useRef();
 
+    /* Estado del mapa */
     const [map, setMap] = useState(null);
+    /* Estado para las coordenadas */
+    const [coords, setCoords] = useState(startPoint);
 
     /* Mostrar el mapa al cargar el componente */
     useEffect(() => {
@@ -26,8 +29,26 @@ export const MapPage = () => {
         setMap(map);
     }, []);
 
+    /* Escuchar cuando se mueve el mapa */
+    useEffect(() => {
+        map?.on('move', () => {
+            const {lng, lat} = map.getCenter();
+            setCoords({
+                lng: lng.toFixed(4),
+                lat: lat.toFixed(4),
+                zoom: map.getZoom().toFixed(2)
+            })
+        });
+
+        return map?.off('move');
+    }, [map]);
+
     return (
         <>
+            <div className="info">
+                Lng: {coords.lng} | lat: {coords.lat} | zoom: {coords.zoom}
+            </div>
+
             <div className="mapContainer"
                  ref={mapDiv}/>
         </>
